1f919fca3551064da0f41e290f4ee316d70f1a2f
kphotchara <kphotchara@gmail.com>
2024-04-24 13:53:48 +0700

    update app.js and app.test.js6

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-config.js
 const crypto = require("crypto");
 const crypto_orig_createHash = crypto.createHash;
 crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
